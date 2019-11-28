62a93ad33feee3eeb02e7a717b20ffefe94c8b28
fabga13 <57445254+fabga13@users.noreply.github.com>
2019-11-28 16:39:44 +0100

    Add files via upload

--- Physical.java
+++ Physical.java
@@ -10,18 +10,41 @@ public class Physical {
 	
 	void movement(Entity e){
 		if(e.getPE_direction() != null) {
-			if(e.getPE_direction() == "Left") { e.setPE_x(e.getPE_x() - e.getPE_vitesse()); }
-			if(e.getPE_direction() == "Right") { e.setPE_x(e.getPE_x() + e.getPE_vitesse());}
-			if(e.getPE_direction() == "Up") { e.setPE_x(e.getPE_y() + e.getPE_vitesse());}
-			if(e.getPE_direction() == "Down") { e.setPE_x(e.getPE_y() - e.getPE_vitesse());}
+			if(e.getPE_direction() == "Left"){
+				move_left(e);
+			}
+			if(e.getPE_direction() == "Right") { 
+				move_right(e);
+			}
+			if(e.getPE_direction() == "Up") { 
+				move_up(e);
+			}
+			if(e.getPE_direction() == "Down") { 
+				move_down(e);
+			}
 		}
+	}
+	
+	void move_left(Entity e) {
+		e.setPE_x(e.getPE_x() - e.getPE_vitesse());
+		e.setShapeLocation(e.getPE_x() - e.getPE_vitesse(), e.getPE_y());
+	}
+	void move_right(Entity e) {
+		e.setPE_x(e.getPE_x() + e.getPE_vitesse());
+		e.setShapeLocation(e.getPE_x() + e.getPE_vitesse(), e.getPE_y());
+	}
+	void move_up(Entity e) {
+		e.setPE_x(e.getPE_x() - e.getPE_vitesse());
+		e.setShapeLocation(e.getPE_x(), e.getPE_y() + e.getPE_vitesse());
+	}
+	void move_down(Entity e) {
+		e.setPE_x(e.getPE_x() - e.getPE_vitesse());
+		e.setShapeLocation(e.getPE_x(), e.getPE_y() - e.getPE_vitesse());
 	}
 	
 	void stop(Entity e) {
 		e.setPE_x(0);
 	}
-	
-	
 	
 	
 	

4f2d3fcc684d6dc8a31a21f4f484e4aa6df31dcf
fabga13 <57445254+fabga13@users.noreply.github.com>
2019-12-03 16:10:56 +0100

    Add files via upload

--- Core.java
+++ Core.java
@@ -1,29 +1,13 @@
 package Controller;
 
 import java.util.ArrayList;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import Model.Entity;
 import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
-import javafx.application.Application;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.effect.BlendMode;
-import javafx.scene.effect.BoxBlur;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.StrokeType;
-import javafx.stage.Stage;
+import javafx.event.ActionEvent;
 import javafx.util.Duration;
-import static java.lang.Math.random;
+
 
 public class Core {
 	
@@ -42,16 +26,19 @@ public class Core {
 	public void set_nbr_Lives(int nbr_Lives) {
 		this.nbr_Lives = nbr_Lives;
 	}
-
-	Timeline fiveSecondsWonder = new Timeline(new KeyFrame(Duration.seconds(5), new EventHandler<ActionEvent>() {
-
+	
+	
+	Timeline fiveSecondsWonder = new Timeline(new KeyFrame(Duration.millis(1), new EventHandler<ActionEvent>() {
 	    @Override
 	    public void handle(ActionEvent event) {
 	        System.out.println("this is called every 5 seconds on UI thread");
 	    }
 	}));
-	fiveSecondsWonder.setCycleCount(Timeline.INDEFINITE);
-	fiveSecondsWonder.play();
+	void timer() {
+		fiveSecondsWonder.setCycleCount(Timeline.INDEFINITE);
+		fiveSecondsWonder.play();
+	}
+	
 	
 }
 
